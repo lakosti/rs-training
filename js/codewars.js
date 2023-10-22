13e0f8a8b391a140c55c85d5ec79d534c1d2de96
Yaroslava <yaroslava.hovenko@gmail.com>
2023-10-22 13:17:10 +0300

    even num

---
+++ js/codewars.js
@@ -0,0 +1,12 @@
+//TASK 1 EVEN OR ODD_____________________________
+
+function testEven(n) {
+    //  return !(n%2) ? true: false
+    return !(n%2)
+}
+console.log(testEven(1));
+console.log(testEven(2));
+console.log(testEven(0));
+console.log(testEven(0.5));
+console.log(testEven(1));
+console.log(testEven(-4));
