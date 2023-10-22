9b571b7b659ec1cc46f2d70750bd5110a2c797ea
Yaroslava <yaroslava.hovenko@gmail.com>
2023-10-22 13:43:16 +0300

    return negative num

--- js/codewars.js
+++ js/codewars.js
@@ -1,12 +1,23 @@
 //TASK 1 EVEN OR ODD_____________________________
 
-function testEven(n) {
-    //  return !(n%2) ? true: false
-    return !(n%2)
-}
-console.log(testEven(1));
-console.log(testEven(2));
-console.log(testEven(0));
-console.log(testEven(0.5));
-console.log(testEven(1));
-console.log(testEven(-4));
+// function testEven(n) {
+//     //  return !(n%2) ? true: false
+//     return !(n%2)
+// }
+// console.log(testEven(1));
+// console.log(testEven(2));
+// console.log(testEven(0));
+// console.log(testEven(0.5));
+// console.log(testEven(1));
+// console.log(testEven(-4));
+
+//TASK 2 RETURN NEGATIVE NUM________________________________
+
+// function makeNegative(num) {
+//     return num > 0 ? - num: num
+// }
+// console.log(makeNegative(5));
+// console.log(makeNegative(-5));
+// console.log(makeNegative(0));
+
+//TASK 3
