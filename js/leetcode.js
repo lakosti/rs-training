05d52b2074a0e8ec45101b12d2bbd4a05763e748
Yaroslava <yaroslava.hovenko@gmail.com>
2023-10-23 23:39:46 +0300

    task from leetcode

---
+++ js/leetcode.js
@@ -0,0 +1,14 @@
+//ЗАДАЧА 1 повернути індекси тих чисел які становлять суму таргету________________________
+
+// let twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
+// console.log(twoSum([1, 2, 4], 3)); // 0 1
+// console.log(twoSum([5, 6, 3], 8)); // 0 2
