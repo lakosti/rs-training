4a71bd631e6afb5b9e5c04ce51170365914420f8
Yaroslava <yaroslava.hovenko@gmail.com>
2023-10-23 21:10:33 +0300

    add three tasks

--- js/codewars.js
+++ js/codewars.js
@@ -20,4 +20,34 @@
 // console.log(makeNegative(-5));
 // console.log(makeNegative(0));
 
-//TASK 3
+//TASK 3 to get /  return number from string_____________________________
+
+// function getAge(inputString) {
+//   return Number(inputString.replace(/\D/g, ''));
+// }
+
+//more simply
+
+// function getAge(inputString) {
+//   return parseInt(inputString);
+// }
+
+// console.log(getAge('years 5 old')); // NaN
+// console.log(getAge('4 years old')); // 4
+
+//TASK 4 The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise________________________________________________
+
+// function setAlarm(employed, vacation) {
+//   return employed && !vacation;
+// }
+// console.log(setAlarm(true, false)); // true
+// console.log(setAlarm(true, true)); // false
+// console.log(setAlarm(false, true)); // false
+
+//TASK 5 to get
+// const areaOrPerimeter = function (l, w) {
+//   return l === w ? l * w : (l + w) * 2;
+// };
+
+// console.log(areaOrPerimeter(3, 3));
+// console.log(areaOrPerimeter(6, 10));
